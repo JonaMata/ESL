87e633873dc77552af32dd952b4f3065bf6bac1f
jmeussen <j.w.meussen@student.utwente.nl>
2026-05-22 09:11:13 +0200

    improved progfpga

--- jiwy_icoboard/progfpga.sh
+++ jiwy_icoboard/progfpga.sh
@@ -2,6 +2,6 @@ sudo modprobe spi-bcm2835 -r
 yosys -p 'synth_ice40 -top jiwy_icoboard -json ice40.json' jiwy_icoboard.v encoder.v pwm.v 
 nextpnr-ice40 --hx8k --json ice40.json --pcf ico-jiwy.pcf --asc ice40.asc
 icepack ice40.asc ice40.bin
-../../iceprog/iceprog -R
+../../icoprog/icoprog -R
 ../../icoprog/icoprog -p < ice40.bin
 sudo modprobe spi-bcm2835
